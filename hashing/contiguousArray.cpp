17b3e77d1dd127f914b2dde27388926580167dde
Will Ondrik <wondrik@my.bcit.ca>
2024-07-12 07:11:54 -0700

    O(n) solution for the contigious array problem.

--- hashing/contiguousArray.cpp
+++ hashing/contiguousArray.cpp
@@ -1,16 +1,16 @@
 #include <vector>
-#include <map>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
     public:
         int findMaxLength(vector<int>& nums) {
-            map<int, int> map;
+            unordered_map<int, int> map;
             int count = 0;
             int maxLen = 0;
 
             // insert count of 0 at index -1
-            map.insert({ 0, -1 });
+            map[0] = -1;
 
             // add 0's and 1's to the count to find valid lengths
             for (int i = 0; i < nums.size(); i++) {
@@ -19,14 +19,12 @@ class Solution {
                 // check if the count is present in the map
                 // if it is, compare max values
                 // if not, insert 
-                auto it = map.find(count);
-                if (it != map.end()) {
-                    maxLen = max(maxLen, i - it->first);
+                if (map.find(count) != map.end()) {
+                    maxLen = max(maxLen, i - map[count]);
                 } else {
-                    map.insert(count, i);
+                    map[count] = i;
                 }
             }
             return maxLen;
-           
         }
 };
